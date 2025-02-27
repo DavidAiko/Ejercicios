a60decd15b622980248529c9b9792ad6fe84f258
DavidAiko <kenx@fall.cat>
2025-02-27 17:45:36 -0500

    Se añadio el código de adivinanza de números

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,28 @@
 import java.util.Random;
-    public class Main {
-        public static void main(String[] args) {
-            int max = 10;
-            int min = 1;
-            int contador1 = 0;
-            int contador2 = 0;
-            int rondas;
-            Random ronda = new Random();
-            rondas = ronda.nextInt((max - min + 1)) + min;
-            System.out.println("-----------------------------------");
-            System.out.println("Rondas: " + rondas);
-            System.out.println("-----------------------------------");
-            int rondadado = rondas;
-             for(int i = 1; i <= rondas; i++) {
-                 int dado1;
-                 int dado2;
-                 Random dados = new Random();
-                 dado1 = dados.nextInt((max - min + 1)) + min;
-                 contador1 += dado1;
-                 dado2 = dados.nextInt((max - min - 6)) + min;
-                 contador2 += dado2;
-                 System.out.println("Ronda " + i + " Dado 1: " + dado1 + "  | Ronda " + i + " Dado 2: " + dado2);
-             }
-            System.out.println("-----------------------------------");
-            System.out.println("Total 1: " + contador1);
-            System.out.println("Total 2: " + contador2);
-            System.out.println("-----------------------------------");
-            if(contador1 <= contador2){
-                System.out.println("DADO 2 GANADOR");
-                System.out.println("-----------------------------------");
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int max = 50;
+        int min = 1;
+        int i = 1;
+        int numeros;
+        int adivinado;
+        int numeroadivinado = 0;
+        int numerorandom = 0;
+        Random numero = new Random();
+        numeros = numero.nextInt((max - min + 1)) + min;
+        numerorandom += numeros;
+        System.out.println("Inserte el Número: ");
+        while (i <= 3) {
+            Scanner intento = new Scanner(System.in);
+            adivinado = intento.nextInt();
+            System.out.println("Inserte el Número: ");
+            if (adivinado == numerorandom) {
+                System.out.println("Número Adivinado!");
+                break;
             } else {
-                System.out.println("DADO 1 GANADOR");
-                System.out.println("-----------------------------------");
+                i++;
             }
         }
     }
+}
