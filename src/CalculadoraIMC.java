f8a9276ea8d5a8bf492c932cb1150c347922879b
David Aiko 👑 <kenx@fall.cat>
2025-02-28 16:30:43 -0500

    Update and rename Ejercicio3.java to CalculadoraIMC.java

--- src/CalculadoraIMC.java
+++ src/CalculadoraIMC.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Ejercicio3 {
+public class CalculadoraIMC {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String respuesta;
